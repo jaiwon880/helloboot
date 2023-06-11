ccfbe7237fd64a041454dd3d86321ff79be9cd02
Han Marie <71927533+jaiwon880@users.noreply.github.com>
2023-06-11 21:09:11 +0900

    Spring Container

--- helloboot/src/main/java/tobyspring/helloboot/HelloController.java
+++ helloboot/src/main/java/tobyspring/helloboot/HelloController.java
@@ -3,11 +3,13 @@ package tobyspring.helloboot;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class HelloController {
-    @GetMapping("/hello")
-    public String hello(String name) { // String -> plain
-        return "Hello " + name;
+import java.util.Objects;
 
+public class HelloController {
+    public String hello(String name) { // String -> plain
+        SimpleHelloService helloService = new SimpleHelloService();
+
+        // null 일 때, 파라미터 전달이 잘못되었을 때
+        return helloService.sayHello(Objects.requireNonNull(name));
     }
 }
--- helloboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ helloboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -4,6 +4,11 @@ import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactor
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.support.GenericApplicationContext;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
@@ -14,23 +19,41 @@ import java.io.IOException;
 
 public class HellobootApplication {
     public static void main(String[] args) {
+        // 스프링 컨테이너
+        GenericApplicationContext applicationContext= new GenericApplicationContext();
+        // Bean 등록
+        applicationContext.registerBean(HelloController.class);
+        applicationContext.refresh(); // Bean Object를 만듦.
+
         // 톰캣없이 서블릿 구현하기
         // 빈 서블릿 컨테이너 구현
         ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
         // 웹서버 생성
         // 람다식 변환
         WebServer webServer = serverFactory.getWebServer(servletContext -> {
-            servletContext.addServlet("hello", new HttpServlet() {
+            servletContext.addServlet("frontcontroller", new HttpServlet() {
                 // service 메소드: 웹프로그래밍 -> 웹 요청을 받아 응답하는 것
                 @Override
                 protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-                    // 정상 응답
-                    resp.setStatus(200);
-                    resp.setHeader("Content-Type", "text/plain");
-                    // Body
-                    resp.getWriter().println("Hello Servlet");
+                    // 인증, 보안, 다국어, 공통 기능
+                    // Mapping: 요청을 가지고 함. Method, Path, Header, Body
+                    // /hello로 들어왔는데 POST 방식 등이 들어오면 처리할 수 없다
+                    if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
+                        String name = req.getParameter("name");
+
+                        HelloController helloController = applicationContext.getBean(HelloController.class);
+                        String ret = helloController.hello(name);
+
+                        // resp.setStatus(HttpStatus.OK.value()); // default 200 값
+                        resp.setContentType(MediaType.TEXT_PLAIN_VALUE);
+                        // Body
+                        resp.getWriter().println(ret);
+                    } else {
+                        resp.setStatus(HttpStatus.NOT_FOUND.value());
+                    }
                 }
-            }).addMapping("/hello");
+                //모든 요청을 서블릿이 컨트롤러를 하겠다. -> frontcontroller
+            }).addMapping("/*");
 
         });
         // Tomcat 서블릿 컨테이너 동작
