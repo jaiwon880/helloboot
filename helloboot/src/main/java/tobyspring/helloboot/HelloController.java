7dcccdf67915f546b3b364fb3c7a4ee4a0c15b07
Han Marie <71927533+jaiwon880@users.noreply.github.com>
2023-06-12 03:53:01 +0900

    어노테이션 매핑 정보 사용

--- helloboot/src/main/java/tobyspring/helloboot/HelloController.java
+++ helloboot/src/main/java/tobyspring/helloboot/HelloController.java
@@ -1,11 +1,13 @@
 package tobyspring.helloboot;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Objects;
 
-@RestController
+@RequestMapping("/hello") // Class 레벨에서의 URL
 public class HelloController {
     private final HelloService helloService;
 
@@ -13,8 +15,9 @@ public class HelloController {
         this.helloService = helloService;
     }
 
-    @GetMapping("/hello") // Get Method로 들어오는 것 중 "/" URL 지정
-    // @RequestMapping(value="/hello", method = RequestMethod.GET)
+    @GetMapping // Get Method로 들어오는 것 중 "/" URL 지정
+    // @RequestMapping(value="/hello", method = RequestMethod.GET) 이전에 쓰던 방식
+    @ResponseBody
     public String hello(String name) { // String -> plain
 
         // null 일 때, 파라미터 전달이 잘못되었을 때
